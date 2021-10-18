3d8d2dd8f31b508352fc99ed09d9be23dee94796
Ali <mohamadalitellawi@gmail.com>
2021-10-18 14:46:38 +0300

    Revit Addins by .net5

---
+++ Mat.RevitCommands/GetElementId.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.Attributes;
+
+namespace Mat.RevitCommands
+{
+    [TransactionAttribute(TransactionMode.ReadOnly)]
+    public class GetElementId : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            //Get UIDocument
+            UIDocument uidoc = commandData.Application.ActiveUIDocument;
+
+            try
+            {
+                //Pick Object
+                Reference pickedObj = uidoc.Selection.PickObject(Autodesk.Revit.UI.Selection.ObjectType.Element);
+
+                //Display Element Id
+                if (pickedObj != null)
+                {
+                    TaskDialog.Show("Element Id", pickedObj.ElementId.ToString());
+                }
+
+                return Result.Succeeded;
+            }
+            catch (Exception e)
+            {
+                message = e.Message;
+                return Result.Failed;
+            }
+            
+        }
+    }
+}
